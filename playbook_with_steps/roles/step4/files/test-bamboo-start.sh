e392c54a0d93aedeab6cccf47c6cbac90363483d
skowpio <skowpio@gmail.com>
2017-07-13 10:37:04 +0200

    stop

---
+++ playbook_with_steps/roles/step4/files/test-bamboo-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-bamboo/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1
+
